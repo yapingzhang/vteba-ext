2f5e40e8af0d555c3d04a5867b13c5f861d1e0e0
skmbw <tongku2008@126.com>
2014-08-06 10:36:33 +0800

    代码生成工具

--- vteba-ext/src/test/java/com/vteba/test/TestCoder.java
+++ vteba-ext/src/test/java/com/vteba/test/TestCoder.java
@@ -2,12 +2,13 @@ package com.vteba.test;
 
 import com.vteba.ext.codegen.CodeBuilder;
 import com.vteba.ext.codegen.KeyType;
+import com.vteba.ext.codegen.Temp;
 
 public class TestCoder {
 
 	public static void main(String[] args) {
 		String rootPath = "D:\\Documents\\GitHub\\vteba-ext\\vteba-ext\\";
-		CodeBuilder builder = new CodeBuilder(rootPath);
+		CodeBuilder builder = new CodeBuilder(rootPath, Temp.Basic);
 		builder.schema("skmbw")
 		.className("EmpUser")
 		.keyType(KeyType.Integer)
